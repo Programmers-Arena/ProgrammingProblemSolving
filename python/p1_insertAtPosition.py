bfe9dd1221f7ef6367765c82a8a11dacddd81933
Divya2k05 <divyaat2005@gmail.com>
2022-10-21 23:29:35 +0530

    updated python

--- python/p1_insertAtPosition.py
+++ python/p1_insertAtPosition.py
@@ -1,30 +1,35 @@
-def insert_at_position(numlist, num):
-  # Write your code here ...
-  
-  pass
+def insert_at_position(list, n):
 
+    index = len(list)
+    for i in range(len(list)):
+        if list[i] > n:
+            index = i
+            break
+    if index == len(list):
+        list = list[:index] + [n]
+    else:
+        list = list[:index] + [n] + list[index:]
+    return list
 
 
 n = None
 
-nlist = [2, 6, 9, 13, 15, 18] # Input1
+list = [2, 6, 9, 13, 15, 18]  # Input1
 n = 11  # Input2
 # Comment Input1 and Input2 to provide your inputs
 
-if(len(nlist)==0):
-  x = int(input("Enter number of elements: "))
-  print("Enter elements in vector: ")
-  for i in range(0,len(nlist)):
-    a = int(input())
-    nlist.append(a)
+if (len(list) == 0):
+    x = int(input("Enter number of elements: "))
+    print("Enter elements in vector: ")
+    for i in range(0, len(list)):
+        a = int(input())
+        list.append(a)
 
 
-if(n==None):
-  n = int(input("Enter number to insert: "))
+if (n == None):
+    n = int(input("Enter number to insert: "))
 
-nlist = insert_at_position(nlist, n)
-print(nlist)
+list = insert_at_position(list, n)
+print(list)
 
 # Expected Output : [2, 6, 9, 11, 13, 15, 18]
-
-
