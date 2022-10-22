0a885cdf7bcfb0631393ec6dda0447ee55b7cc58
Joydeep Mallick <94801952+JoydeepMallick@users.noreply.github.com>
2022-10-22 23:21:35 +0530

    Add files via upload

--- c++/p2_numberGames.cpp
+++ c++/p2_numberGames.cpp
@@ -1,31 +1,34 @@
 /*
 Question Description:
-
 Given a list "numlist" of numbers, perform the following operations:
 If number is completely divisible by 3, then add 5 in it.
 Otherwise if, number is even and at the even position then insert 2 at the end in list.
 Otherwise, make the element 0.
 */
-
 #include <iostream>
 #include <vector>
 using namespace std;
-
 vector<int> numberGames(vector<int> nlist)
 {
   // Write your code here ...
+  int len = nlist.size();
+  for(int i = 0; i < len; i++){
+      if(nlist[i]%3 == 0) nlist[i]+=5;
+      //pos = ind+1
+      else if(nlist[i]%2==0 && i%2) nlist.push_back(2);
+      else nlist[i] = 0;
+      //len = nlist.size();
+  }
+  return nlist;
 }
-
-
-
 int main()
 {
   vector<int> numlist;
-  int x,a;
-  
+ int x,a;
+
   numlist = {3, 12, 7, 20, 15, 19}; // Input1
   // Comment Input1 to provide your inputs
-  
+
   if(numlist.size()==0)
   {
     cout<<"Enter number of elements: \n";
