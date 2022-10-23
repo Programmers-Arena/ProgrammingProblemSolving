b935a82296806ea76a3f9c2385460fd417a398a0
Amit <52880868+AvTe@users.noreply.github.com>
2022-10-23 12:49:11 +0530

    Left Time Calculator

---
+++ python/left_time_calculater.py
@@ -0,0 +1,17 @@
+
+
+x = int(input("Enter your present age:"))
+
+d = (x*365)
+w = (x*52)
+m = (x*12)
+
+n4 = {90*365-d}
+
+n5 = {90*52-w}
+n6 = {90*12-m}
+
+a = (f"You have {n4},  days {n5} weeks and {n6} months left .")
+print(a)
+
+ 
