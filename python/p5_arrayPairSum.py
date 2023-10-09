f2475500a263696c5199b75a42ae6af6c0443bc3
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-09 17:10:30 +0530

    Create p5_arrayPairSum.py

---
+++ python/p5_arrayPairSum.py
@@ -0,0 +1,14 @@
+"""
+Find the pairs from array, whose minimum element sum will produce the maximum possible output.
+Output the maximum possible sum.
+
+!! Do not hard code !!
+"""
+
+arr1 = [3,9,8,6]           # Output: 11
+arr2 = [6,2,8,4,7,1]       # Output: 12
+# Start code from below:
+
+
+
+
