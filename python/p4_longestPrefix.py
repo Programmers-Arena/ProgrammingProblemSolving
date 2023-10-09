fc0bec00d0c5d0a855120c893f0e6ff1c1cf5551
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-09 15:29:52 +0530

    Create p4_longestPrefix.py

---
+++ python/p4_longestPrefix.py
@@ -0,0 +1,14 @@
+"""
+Given a list of strings, you need to find the longest prefix among all of them and return the prefix.
+If no prefix found then simply return 0.
+All strings should be in capital letters.
+
+!! Do not hard code !!
+"""
+
+arr1 = ["RAIL","RAIN","RAM"]           # Output: ra
+arr2 = ["STORE","FIRE","STOVE"]        # Output: 0
+# Start code from below:
+
+
+
