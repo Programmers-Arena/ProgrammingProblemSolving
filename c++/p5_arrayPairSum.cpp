5f894a15a87a80cb8f385fd946fb6cfa5cfccadd
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-09 17:08:48 +0530

    Create p5_arrayPairSum.cpp

---
+++ c++/p5_arrayPairSum.cpp
@@ -0,0 +1,21 @@
+/*
+Find the pairs from array, whose minimum element sum will produce the maximum possible output.
+Output the maximum possible sum.
+
+!! Do not hard code !!
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr1 = {3,9,8,6};       // Output: 11
+    vector<int> arr2 = {6,2,8,4,7,1};   // Output: 12
+    // Start code from below:
+
+
+
+
+return 0;
+}
