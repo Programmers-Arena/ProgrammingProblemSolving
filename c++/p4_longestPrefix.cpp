5b173b6d953960a429f09a76d321ecfd24ceb5d2
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-09 15:11:58 +0530

    Create p4_longestPrefix.cpp

---
+++ c++/p4_longestPrefix.cpp
@@ -0,0 +1,22 @@
+/*
+Given an array of strings, you need to find the longest prefix among all of them and return the prefix.
+If no prefix found then simply return 0.
+All strings should be in capital letters.
+
+Output: fl
+
+!! Do not hard code !!
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<string> arr = {"flower","flow","flex"};
+    // Start code from below:
+
+
+
+return 0;
+}
