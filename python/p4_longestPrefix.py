f005942b68e2694f8b7b94698ba9dc2e59a681c4
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2023-10-16 22:53:00 +0530

    Update p4_longestPrefix.py

--- python/p4_longestPrefix.py
+++ python/p4_longestPrefix.py
@@ -10,5 +10,25 @@ arr1 = ["RAIL","RAIN","RAM"]           # Output: ra
 arr2 = ["STORE","FIRE","STOVE"]        # Output: 0
 # Start code from below:
 
+s = arr1[0];
+x=0
+y=0
+for i in range(0,len(arr1)):
+    if(len(arr1[i])<len(s)):
+        x = len(arr1[i])-1
+    else:
+        x = len(s)-1
+    while(y<=x):
+        if(s[y]!=arr1[i][y]):
+            break
+        y+=1
+    if(len(s)<=0):
+        s = "0"
+        break
+    else:
+        s = s[0:y]
+    x = 0
+    y = 0
+print(s);
 
 
