4a34de1fbd686872c32b7e55b7767e7a62c3011c
Adars T S <117556787+a6ar55@users.noreply.github.com>
2023-10-20 20:50:12 +0530

    Update p8_pairWhichGivesGivenDifference.py

--- python/p8_pairWhichGivesGivenDifference.py
+++ python/p8_pairWhichGivesGivenDifference.py
@@ -10,5 +10,29 @@ size = len(arr)
 diff = 45
 
 # Start your code from here
+def find_pairs_with_difference(arr, diff):
+    arr.sort()  # Sort the arr
+
+    p1 = 0
+    p2 = 1
+    count = 0
+    n = len(arr)
+    pairs = []
+
+    while p2 < n and p1 < n:
+        if arr[p2] - arr[p1] == diff:
+            count += 1
+            pairs.append((arr[p1], arr[p2]))  # Store the pair
+            p1 += 1
+            p2 += 1
+        elif arr[p2] - arr[p1] > diff:
+            p1 += 1
+        else:
+            p2 += 1
+
+    return count, pairs
+result, pairs = find_pairs_with_difference(arr, diff)
+print(f"Number of unique pairs with a difference of {diff}: {result}")
+print(f"Pairs with a difference of {diff}: {pairs}")
 
   
