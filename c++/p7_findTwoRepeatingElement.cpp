23dca85d0a06c71b71acb983218a92ef99d1509d
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-20 16:04:29 +0530

    Create p7_findTwoRepeatingElement.cpp

---
+++ c++/p7_findTwoRepeatingElement.cpp
@@ -0,0 +1,19 @@
+/*
+Write the code to find the two repeating elements in array.
+Array is provided.
+
+!! Do not hard code !!
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = { 4, 2, 4, 5, 2, 3, 1 };
+    int size = arr.size();
+   
+    // Start writing code from here
+  
+    return 0;
+}
