e44fae34a2f3efbee32d3997ce5f70b367044699
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-20 16:11:04 +0530

    Create p7_findTwoRepeatingElement.py

---
+++ python/p7_findTwoRepeatingElement.py
@@ -0,0 +1,10 @@
+"""
+Write the code to find the two repeating elements in array.
+Array is provided.
+
+!! Do not hard code !!
+"""
+
+arr = [ 5, 0, 2, -3, 3, 1, 6, 4, 2, 0 ]    # Output: Repeating - 0, 2
+
+# Start writing code from here
