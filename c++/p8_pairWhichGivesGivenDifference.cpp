96ac3a9235c232220afaa5bcbd0f164e588b1ae3
Adars T S <117556787+a6ar55@users.noreply.github.com>
2023-10-20 21:01:45 +0530

    Update p8_pairWhichGivesGivenDifference.cpp
    solved the problem in C++

--- c++/p8_pairWhichGivesGivenDifference.cpp
+++ c++/p8_pairWhichGivesGivenDifference.cpp
@@ -7,15 +7,48 @@ You are given difference and an array.
 
 #include <iostream>
 #include<bits/stdc++.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+pair<int, vector<pair<int, int>>> findPairsWithDifference(vector<int>& arr, int diff) {
+    sort(arr.begin(), arr.end());
+
+    int p1 = 0;
+    int p2 = 1;
+    int count = 0;
+    int n = arr.size();
+    vector<pair<int, int>> pairs;
+
+    while (p2 < n && p1 < n) {
+        if (arr[p2] - arr[p1] == diff) {
+            count++;
+            pairs.push_back(make_pair(arr[p1], arr[p2]));
+            p1++;
+            p2++;
+        } else if (arr[p2] - arr[p1] > diff) {
+            p1++;
+        } else {
+            p2++;
+        }
+    }
+
+    return make_pair(count, pairs);
+}
 int main() {
     vector<int> arr = { 18, 49, 86, 12, 41, 32, 56 };  // Output: 86, 41
     int size = arr.size();
     int diff = 45;
 
     // Start your code from here
+    auto result = findPairsWithDifference(arr, diff);
+    cout << "Number of unique pairs with a difference of " << diff << ": " << result.first << endl;
 
+    cout << "Pairs with a difference of " << diff << ": ";
+    for (const auto& pair : result.second) {
+        cout << "(" << pair.first << ", " << pair.second << ") ";
+    }
+    cout << endl;
   
 return 0;
 }
