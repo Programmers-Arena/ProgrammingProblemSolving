e55e76186f4c2333294ff0c36f4a899f700c972f
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-20 15:30:24 +0530

    Update p6_LargestContinuousArraySum.py

--- python/p6_LargestContinuousArraySum.py
+++ python/p6_LargestContinuousArraySum.py
@@ -5,6 +5,6 @@ Array will contain negative elements as well.
 !! Do not hard code !!
 """
 
-arr = { 0, 4, -7, -3, 8, -9, 1, 6, 5, -2 };  # Output: 12
+arr = [ 0, 4, -7, -3, 8, -9, 1, 6, 5, -2 ]  # Output: 12
 
 # Start code from here
