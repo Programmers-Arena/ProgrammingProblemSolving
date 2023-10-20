b7fbe7b5bb7e74883e40af265a6f3f943ef580c3
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2023-10-20 16:36:43 +0530

    Solved p7_findTwoRepeatingElement.py
    Provided correct solution of the problem7

--- python/p7_findTwoRepeatingElement.py
+++ python/p7_findTwoRepeatingElement.py
@@ -5,6 +5,15 @@ Array is provided.
 !! Do not hard code !!
 """
 
-arr = [ 5, 0, 2, -3, 3, 1, 6, 4, 2, 0 ]    # Output: Repeating -> 0, 2
+arr = [ 5, 0, 2, -3, 3, 1, 6, 4, 2, 0 ] # Output: Repeating -> 0, 2
 
 # Start writing code from here
+
+size = len(arr)
+
+print("Repeating -> ",end="")
+for i in range(0,size):
+  for j in range(i+1,size):
+    if arr[i] == arr[j]:
+      print(str(arr[i])+" ",end="")
+      break
