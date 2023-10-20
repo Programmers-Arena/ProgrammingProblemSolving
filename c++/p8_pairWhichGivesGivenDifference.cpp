413d43ec91ed608b44f6aac951cff8d3c8a1e249
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-20 16:52:59 +0530

    Create p8_pairWhichGivesGivenDifference.cpp

---
+++ c++/p8_pairWhichGivesGivenDifference.cpp
@@ -0,0 +1,20 @@
+/*
+Find the pair which produces given difference from array.
+You are given difference and an array.
+
+!! Do not hard code !!
+*/
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = { 1, 8, 30, 40, 100 };
+    int size = arr.size();
+
+    // Start your code from here
+
+  
+return 0;
+}
