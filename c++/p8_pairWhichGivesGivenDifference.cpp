2e917aacd1c81a30d540c58619905ffc5fdeac49
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-20 16:57:07 +0530

    Update p8_pairWhichGivesGivenDifference.cpp

--- c++/p8_pairWhichGivesGivenDifference.cpp
+++ c++/p8_pairWhichGivesGivenDifference.cpp
@@ -10,8 +10,9 @@ You are given difference and an array.
 using namespace std;
 
 int main() {
-    vector<int> arr = { 1, 8, 30, 40, 100 };
+    vector<int> arr = { 18, 49, 86, 12, 41, 32, 56 };  // Output: 86, 41
     int size = arr.size();
+    int diff = 45;
 
     // Start your code from here
 
