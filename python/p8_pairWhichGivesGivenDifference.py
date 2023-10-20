7b70f1e7589cb0a8246fe6c2b620075ea8b26699
Akash Kumar <114604164+Akash88585@users.noreply.github.com>
2023-10-20 16:56:16 +0530

    Create p8_pairWhichGivesGivenDifference.py

---
+++ python/p8_pairWhichGivesGivenDifference.py
@@ -0,0 +1,14 @@
+"""
+Find the pair which produces given difference from array.
+You are given difference and an array.
+
+!! Do not hard code !!
+"""
+
+arr = [ 18, 49, 86, 12, 41, 32, 56 ]  # Output: 86, 41
+size = len(arr)
+diff = 45
+
+# Start your code from here
+
+  
