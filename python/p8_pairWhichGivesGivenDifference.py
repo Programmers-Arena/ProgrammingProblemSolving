ce652a84d86a01cad35e21cfc6e3aed34558ae46
Shreekantha3 <biradarshreekantha@gmail.com>
2023-10-20 10:00:21 -0500

    fix/#51 differance

--- python/p8_pairWhichGivesGivenDifference.py
+++ python/p8_pairWhichGivesGivenDifference.py
@@ -5,10 +5,32 @@ You are given difference and an array.
 !! Do not hard code !!
 """
 
-arr = [ 18, 49, 86, 12, 41, 32, 56 ]  # Output: 86, 41
-size = len(arr)
-diff = 45
+# arr = [ 18, 49, 86, 12, 41, 32, 56 ]  # Output: 86, 41
+# size = len(arr)
+# diff = 45
 
 # Start your code from here
 
   
+def find_pair_with_difference(arr, diff):
+    element_indices = {}
+
+    for i, num in enumerate(arr):
+        complement = num + diff
+
+        if complement in element_indices:
+            return [num, complement]
+
+        element_indices[num] = i
+
+    return None
+
+arr = [18, 49, 86, 12, 41, 32, 56]
+diff = 45
+
+result = find_pair_with_difference(arr, diff)
+
+if result:
+    print("Pair with difference", diff, "is:", result)
+else:
+    print("No such pair found.")
